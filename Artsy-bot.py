a77989a1c0272432ce734957e62db07e32abe633
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2021-03-22 15:12:20 +0000

    Init Repo

---
+++ Artsy-bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from discord.utils import get
+from roles import ROLES
+import asyncio  
+
+bot = discord.Client()
+
+botTesting=bot.get_channel(821435572891287603)
+rules=bot.get_channel(821433342632788018)
+
+@bot.event
+async def on_ready():
+    print("Logged in as {0.user}".format(bot))
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    await rules.send()
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('?React'):
+        await message.add_reaction("👍")
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    channel = reaction.message.channel
+    await channel.send('{} has added {} to the message: {}'.format(user.name, reaction.emoji, reaction.message.content))
+    print(user.name,reaction.emoji)
+    if reaction.emoji == "👍":
+        role = get(member.roles, name="Test")
+        await bot.add_roles(user.name, role)
+
+
+bot.run('ODIxNDQwOTcyNzMzODA4NzEy.YFDwhQ.eKzk5nhM5ieq4QpOXkJNegvwXaE')   
---
+++ roles.py
@@ -0,0 +1,18 @@
+ROLES={
+    "Pronouns":{
+        0,"She/Her",
+        1,"He/Him",
+        2,"She/They",
+        3,"He/They",
+        4,"They/Them",
+        5,"Other"
+    },
+    "Courses":{
+        0,"Fine Art",
+        1,"Graphic Design",
+        2,"Fashion",
+        3,"Illustration",
+        4,"Film and Tv",
+        5,"Animation",
+    }
+}
